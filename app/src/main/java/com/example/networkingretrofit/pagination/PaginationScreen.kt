a1b6674e5b17e782b140cbf05304984089752f7a
noliw <noliw2017@gmail.com>
2024-06-21 00:56:08 -0700

    pagination intro

---
+++ app/src/main/java/com/example/networkingretrofit/pagination/PaginationScreen.kt
@@ -0,0 +1,40 @@
+package com.example.networkingretrofit.pagination
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PaginationScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "PaginationScreen") })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PaginationScreenPreview() {
+    PaginationScreen()
+}
